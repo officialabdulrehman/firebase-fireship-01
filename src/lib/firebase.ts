b6abdce7c4d676367a7d878651bb490d5a3e4103
NizTheDev <arehman08@hotmail.com>
2023-01-14 15:38:22 +0500

    feat: added firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhB5i-fEkvHVLNoWeB0DQ1uglQuHs-yeY",
+  authDomain: "fireship-tutorial-2c944.firebaseapp.com",
+  projectId: "fireship-tutorial-2c944",
+  storageBucket: "fireship-tutorial-2c944.appspot.com",
+  messagingSenderId: "193805392865",
+  appId: "1:193805392865:web:cd38129711966591a090a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
